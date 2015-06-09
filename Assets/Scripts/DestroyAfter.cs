01bbb4c26d2931a35d7659ad98be6a9f1b5360fb
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-06-09 23:37:38 +0200

    Little script which destroy any object after a certain time

---
+++ Assets/Scripts/DestroyAfter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfter : MonoBehaviour {
+	public float DestroyTime;
+
+	void Awake() {
+		Destroy (gameObject, DestroyTime);
+	}
+}
