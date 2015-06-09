997f27610400c268a68ebbe763c5732218139319
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-06-09 23:56:44 +0200

    Added very simple camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject FollowWhat;
+	public float FollowSpeed = 5.0f;
+
+	public void Update() {
+		transform.position = Vector3.Lerp (transform.position, new Vector3(FollowWhat.transform.position.x, FollowWhat.transform.position.y, transform.position.z), Time.deltaTime * FollowSpeed);
+	}
+}
