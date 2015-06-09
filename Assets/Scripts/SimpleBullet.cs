74fdb9c0b1c804d1b029c24f543fbfe8b00e104e
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-06-09 23:30:18 +0200

    Explosion with incredible sound effect :D

--- Assets/Scripts/SimpleBullet.cs
+++ Assets/Scripts/SimpleBullet.cs
@@ -5,6 +5,8 @@ public class SimpleBullet : MonoBehaviour {
 	public float BulletLifeTime = 3.0f;
 	private float _startTime;
 
+	public GameObject Explosion;
+
 	public void Awake() {
 		_startTime = Time.time;
 	}
@@ -13,5 +15,16 @@ public class SimpleBullet : MonoBehaviour {
 		if (Time.time - _startTime > BulletLifeTime) {
 			Destroy(gameObject);
 		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == Tags.Wood) {
+			Explode();
+			Destroy (gameObject);
+		}
+	}
+
+	private void Explode() {
+		Instantiate (Explosion, gameObject.transform.position, gameObject.transform.rotation);
 	}
 }
---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Tags {
+	public const string Obstacle = "Obstacle";
+	public const string Wood = "Wood";
+}
