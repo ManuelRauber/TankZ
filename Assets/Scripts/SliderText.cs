0916f275ec617b52fc140a8f0312523fae04aadf
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-06-14 22:06:20 +0200

    Update Slider Text according to the slider values

---
+++ Assets/Scripts/SliderText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour {
+	public string DefaultText;
+	public Text TextObject;
+
+	public void Awake () {
+		TextObject.text = DefaultText;
+	}
+
+	public void SliderValueChanged(float value) {
+		var textValue = value.ToString ();
+
+		TextObject.text = textValue;
+	}
+}
