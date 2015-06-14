f1e3e3367d528ec48c69db0276396ecb6af41024
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-06-14 18:52:53 +0200

    Very simple main menu scene

---
+++ Assets/Scripts/CloseGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseGame : MonoBehaviour {
+
+	public void Start() {
+		if (Application.isWebPlayer || Application.platform == RuntimePlatform.OSXEditor) {
+			gameObject.SetActive(false);
+		}
+	}
+
+	public void OnClick() {
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/GitHubButtonScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GitHubButtonScript : MonoBehaviour {
+
+	public void ButtonClicked() {
+		Application.OpenURL ("https://github.com/ManuelRauber/TankZ");
+	}
+}
