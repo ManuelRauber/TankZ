a476c8aa9d31cdf29988c20ecc31693a809f0f01
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-06-14 22:41:26 +0200

    Use Escape to go back to menu

---
+++ Assets/Scripts/BackToMenuOnEscape.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToMenuOnEscape : MonoBehaviour {
+	public void Update () {
+		if (Input.GetKey (KeyCode.Escape)) {
+			Application.LoadLevel("GenerateNewMapScene");
+		}
+	}
+}
