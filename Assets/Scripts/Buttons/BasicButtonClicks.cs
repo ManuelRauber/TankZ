94895d04b94cc8ff785ad680c5383991de84044c
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-06-14 21:48:06 +0200

    New Scene for creating a new world with parameters (non functional yet)

---
+++ Assets/Scripts/Buttons/BasicButtonClicks.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicButtonClicks : MonoBehaviour {
+
+	public void GitHubButtonClicked() {
+		Application.OpenURL ("https://github.com/ManuelRauber/TankZ");
+	}
+
+	public void LoadLevel(string levelName) {
+		Application.LoadLevel (levelName);
+	}
+}
--- Assets/Scripts/Buttons/GitHubButtonScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GitHubButtonScript : MonoBehaviour {
-
-	public void ButtonClicked() {
-		Application.OpenURL ("https://github.com/ManuelRauber/TankZ");
-	}
-}
