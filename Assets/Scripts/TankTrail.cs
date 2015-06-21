73cc2d8ccf0b1573197784c7d9ce32a3b9f5c686
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-06-21 23:20:08 +0200

    First draft of tank trails

---
+++ Assets/Scripts/TankTrail.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Creating a game object per trail is not the best idea...
+public class TankTrail : MonoBehaviour {
+	public GameObject SingleMarkVisual;
+	public float MarkDistance = 0.18f;
+	public float MarkLifeTime = 5f;
+
+	private float _absoluteMovingDistance;
+	private Vector2 _lastMovePosition;
+
+	public void Awake() {
+		_lastMovePosition = transform.position;
+	}
+
+	public void Update () {
+		var position = transform.position;
+
+		var distance = Vector2.Distance (position, _lastMovePosition);
+
+		_lastMovePosition = position;
+
+		_absoluteMovingDistance += distance;
+
+		if (_absoluteMovingDistance > MarkDistance) {
+			var trail = Instantiate(SingleMarkVisual, position, transform.rotation);
+
+			_absoluteMovingDistance -= MarkDistance;
+
+			Destroy(trail, MarkLifeTime);
+		}
+	}
+}
