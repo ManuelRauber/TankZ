03691affad97891a9f928181a0455474286789fd
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-06-21 23:19:21 +0200

    Prevent annoying errors in the test scene :)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,10 @@ public class CameraFollow : MonoBehaviour {
 	public float FollowSpeed = 5.0f;
 
 	public void Update() {
+		if (FollowWhat == null) {
+			return;
+		}
+
 		transform.position = Vector3.Lerp (transform.position, new Vector3(FollowWhat.transform.position.x, FollowWhat.transform.position.y, transform.position.z), Time.deltaTime * FollowSpeed);
 	}
 }
