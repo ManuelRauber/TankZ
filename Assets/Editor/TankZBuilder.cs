e22d759b3caf97427fc724b5e79830def8ff21f1
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-09-16 00:03:03 +0200

    Added WebGL Builder

---
+++ Assets/Editor/TankZBuilder.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+public class TankZBuilder {
+	private const string MenuPrefix = "TankZ/";
+
+	[MenuItem(MenuPrefix + "Build/WebGL")]
+	public static void WebGlBuild() {
+		string[] scenes = {
+			"Assets/Scenes/MainMenuScene.unity",
+			"Assets/Scenes/GenerateNewMapScene.unity",
+			"Assets/Scenes/OperationWahooka.unity"
+		};
+
+		BuildPipeline.BuildPlayer (scenes, 
+		                          "Build/WebGL",
+		                          BuildTarget.WebGL,
+		                          BuildOptions.None);
+	}
+}
