4761f6a91ec8352af747bb99415d3009550ae83b
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-09-16 20:16:24 +0200

    More comments :)

--- Assets/Editor/TankZBuilder.cs
+++ Assets/Editor/TankZBuilder.cs
@@ -3,14 +3,17 @@
 public class TankZBuilder {
 	private const string MenuPrefix = "TankZ/";
 
+	// Add a menu item, so we can use this code in the editor, too
 	[MenuItem(MenuPrefix + "Build/WebGL")]
 	public static void WebGlBuild() {
+		// Scenes which will be built
 		string[] scenes = {
 			"Assets/Scenes/MainMenuScene.unity",
 			"Assets/Scenes/GenerateNewMapScene.unity",
 			"Assets/Scenes/OperationWahooka.unity"
 		};
 
+		// Add WebGL to the build pipeline
 		BuildPipeline.BuildPlayer (scenes, 
 		                          "Build/WebGL",
 		                          BuildTarget.WebGL,
