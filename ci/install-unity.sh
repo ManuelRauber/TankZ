34373e806d0dafe803b819002c07a04bc0fca5b2
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-09-16 17:05:36 +0200

    Update install-unity.sh
    Testing...

--- ci/install-unity.sh
+++ ci/install-unity.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-exit
-
 echo 'Downloading from http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg: '
 curl -o Unity.pkg http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg
 
