0326f7312fd469d5e4499b0fd60671cbae9a3653
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-09-16 20:13:11 +0200

    Using new Unity Version

--- ci/install-unity.sh
+++ ci/install-unity.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-echo 'Downloading from http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg
+echo 'Downloading from http://netstorage.unity3d.com/unity/e7947df39b5c/MacEditorInstaller/Unity-5.2.0f3.pkg: '
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/e7947df39b5c/MacEditorInstaller/Unity-5.2.0f3.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
