a2952cc41b9091789c8f5b6d6d237d98c5b92013
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-09-17 19:44:56 +0200

    More deployment things

--- ci/install-unity.sh
+++ ci/install-unity.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+if [ ! -d ./unity ]; then
+  mkdir ./unity
+fi
+
 if [ ! -f ./unity/Unity.pkg ]; then
   echo 'Downloading from http://netstorage.unity3d.com/unity/e7947df39b5c/MacEditorInstaller/Unity-5.2.0f3.pkg: '
   curl -o ./unity/Unity.pkg http://netstorage.unity3d.com/unity/e7947df39b5c/MacEditorInstaller/Unity-5.2.0f3.pkg
