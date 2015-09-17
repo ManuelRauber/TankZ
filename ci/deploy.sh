20be91ec0c07d1ff02bc428bef9304535c85d468
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-09-17 19:39:41 +0200

    Travis Caching and deploy.sh

---
+++ ci/deploy.sh
@@ -0,0 +1,7 @@
+rsyncUser=${AZURE_WA_USERNAME}
+rsyncPassword=${AZURE_WA_PASSWORD}
+rsyncHost=${AZURE_WA_SITE}
+
+export RSYNC_PASSWORD=$rsyncPassword
+
+rsync -e 'ssh -ax' -Iav --delete $(pwd)/Build/WebGL $rsyncUser@$rsyncHost:/
