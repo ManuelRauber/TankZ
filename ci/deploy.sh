f16efab4331f74468615d36311d25a12794eda4e
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-09-17 19:43:10 +0200

    Some echoing

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,5 @@
+echo "Deploying to Azure via rsync..."
+
 rsyncUser=${AZURE_WA_USERNAME}
 rsyncPassword=${AZURE_WA_PASSWORD}
 rsyncHost=${AZURE_WA_SITE}
@@ -5,3 +7,5 @@ rsyncHost=${AZURE_WA_SITE}
 export RSYNC_PASSWORD=$rsyncPassword
 
 rsync -e 'ssh -ax' -Iav --delete $(pwd)/Build/WebGL $rsyncUser@$rsyncHost:/
+
+echo "Deployment done! Have fun!"
