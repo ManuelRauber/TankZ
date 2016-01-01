a826f0ac81363ed7c8659e66031a3ed76f130bd5
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-01-01 15:45:10 +0100

    Current Unity Download URL

--- ci/install-unity.sh
+++ ci/install-unity.sh
@@ -7,8 +7,8 @@ fi
 
 # If unity was cached form a previous run, do not download it again
 if [ ! -f ./unity/Unity.pkg ]; then
-  echo 'Downloading from http://netstorage.unity3d.com/unity/e7947df39b5c/MacEditorInstaller/Unity-5.2.0f3.pkg: '
-  curl -o ./unity/Unity.pkg http://netstorage.unity3d.com/unity/e7947df39b5c/MacEditorInstaller/Unity-5.2.0f3.pkg
+  echo 'Downloading from http://netstorage.unity3d.com/unity/cc9cbbcc37b4/MacEditorInstaller/Unity-5.3.1f1.pkg: '
+  curl -o ./unity/Unity.pkg http://netstorage.unity3d.com/unity/cc9cbbcc37b4/MacEditorInstaller/Unity-5.3.1f1.pkg
 else
   echo 'Using cached Unity version'
 fi
